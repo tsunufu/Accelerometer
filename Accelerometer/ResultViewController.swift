afda5f677a1b4faa6e12bf653ab468a7e430ecce
RyosukeFujiwara <tsunufu@icloud.com>
2022-08-26 14:48:42 +0900

    Initial Commit

---
+++ Accelerometer/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  Accelerometer
+//
+//  Created by ryo on 2022/08/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var accelerationX: Double!
+    
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let result: Double = fabs(accelerationX * 100)
+        label.text = String(format: "%.1f°", result)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Accelerometer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Accelerometer
+//
+//  Created by ryo on 2022/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
